6758765d8a7e181cb484617f77f39bf3aba1c8d4
umeshbogati <bogatiu17@gmail.com>
2026-01-09 09:57:31 +0545

    updated.

--- event_project/event_project/urls.py
+++ event_project/event_project/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.authtoken.views import obtain_auth_token 
 
 
 urlpatterns = [
